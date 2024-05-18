a438311b2e248392ccf7d3671dcc811168a0cf08
Anita Shinde <126587206+anushinde2002@users.noreply.github.com>
2024-05-19 02:04:49 +0530

    Add files via upload

---
+++ QRGenrator/QR.py
@@ -0,0 +1,9 @@
+import qrcode
+import image
+qr=qrcode.QRCode(version=15,box_size=10,border=5)
+data="https://www.youtube.com/@shradhaKD"
+
+qr.add_data(data)
+qr.make(fit=True)
+img=qr.make_image(fill="black",back_color="white")
+img.save("test.png")
